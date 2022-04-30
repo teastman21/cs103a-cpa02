d8712732d99f441c19e84c60f3a3e0845da1dce6
teastman21 <71243994+teastman21@users.noreply.github.com>
2022-04-30 19:53:27 -0400

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -5,10 +5,10 @@ const ObjectId = Schema.Types.ObjectId;
 const Mixed = Schema.Types.Mixed;
 
 var courseSchema = Schema( {
-    firstName: String,
-    lastName: String,
-    playerID: Number,
-    teamID: Number,
+    firstName: Mixed,
+    lastName: Mixed,
+    playerID: Mixed,
+    teamID: Mixed,
 } );
 
 module.exports = mongoose.model( 'Course', courseSchema );
